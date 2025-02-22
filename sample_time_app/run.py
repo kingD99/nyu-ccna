1b798f23b3659a065117054f777ce67805eed350
kingD99 <143452248+kingD99@users.noreply.github.com>
2025-02-22 13:10:25 +0530

    Update run.py

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,12 +1,13 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
+@app.route("/time")
+def get_current_time():
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"Current time is: {now}"
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
-
-
-app.run(host='0.0.0.0',
-        port=8080,
-        debug=True)
+if __name__ == "__main__":
+    # Bind to all interfaces and listen on port 80
+    app.run(host="0.0.0.0", port=80)
